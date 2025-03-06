dd4176254c4f2c23fd21ee651409261e027c7569
Timjini <hatim.jini@gmail.com>
2025-03-06 14:35:47 +0100

    Add: detect language using url , localStorage

--- src/i18n.ts
+++ src/i18n.ts
@@ -189,13 +189,19 @@ const resources = {
   },
 };
 
+
 i18n
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
     resources,
     fallbackLng: "en",
+    detection: {
+      order: ["querystring", "localStorage", "navigator"],
+      lookupQuerystring: "lang",
+      lookupLocalStorage: "i18nextLng",
+      caches: ["localStorage"],
+    },
     interpolation: { escapeValue: false },
   });
-
 export default i18n;
